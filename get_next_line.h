4b72ed8c9802c70f4c9582e490baf1d9b1f25453
SexBikiniMad <93735906+SexBikiniMad@users.noreply.github.com>
2022-03-12 12:34:02 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -9,18 +9,15 @@
 # include <stddef.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1000
+#  define BUFFER_SIZE 1000000
 # endif
 
-char	*get_next_line(int fd);
-ssize_t	read_file(int fd, char **buffer, char **buff_read, char **line);
-char	*get_line(char **buff_read, char **line);
-
-// utils
-char	*ft_strchr(const char *s, int c);
-size_t	ft_strlcpy(char *dest, const char *src, size_t destsize);
+char    *get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strdup(const char *s1);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-
+char	*ft_strtrim(char const *s1, char const *set);
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strdup(const char *src);
+char	*ft_strndup(const char *src);
+size_t	ft_strnlen(const char *str);
 #endif
